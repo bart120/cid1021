8df6261284dcd3bc87e5a46f4bd91308dda6ab93
Vincent <vleclerc@inow.fr>
2021-10-25 16:59:00 +0200

    lazy loading

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,13 +1,16 @@
 import HomePage from '../components/pages/HomePage.vue'
-import CarsAddPage from '../components/pages/cars/CarsAddPage.vue'
-import CarsListPage from '../components/pages/cars/CarsListPage.vue'
+//import CarsAddPage from '../components/pages/cars/CarsAddPage.vue'
+//import CarsListPage from '../components/pages/cars/CarsListPage.vue'
 import CarsSearchPage from '../components/pages/cars/CarsSearchPage.vue'
 import CarsLayout from '../components/pages/cars/CarsLayout.vue'
 
+const carsListPage = () => import('../components/pages/cars/CarsListPage.vue');
+const carsAddPage = () => import('../components/pages/cars/CarsAddPage.vue');
+
 export default [
     { path: '/', component: HomePage },
-    { path: '/cars/add', component: CarsAddPage },
-    { path: '/cars/list', component: CarsListPage },
+    { path: '/cars/add', component: carsAddPage /*CarsAddPage*/ },
+    { path: '/cars/list', component: carsListPage /*CarsListPage*/ },
     { path: '/cars/search', component: CarsSearchPage }
     /*{
         path: '/cars', component: CarsLayout,
